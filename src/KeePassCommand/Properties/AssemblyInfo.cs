1f48cfa6208df3ee0cfc8f05c6603bb441a8f87d
Mirco Babin <mirco@tripsoftware.nl>
2020-01-28 16:56:25 +0100

    Recreate Version 2.1, using GlobalAssemblyInfo.cs to properly stamp all projects with the same version number.

--- src/KeePassCommand/Properties/AssemblyInfo.cs
+++ src/KeePassCommand/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d584fe0a-f840-461e-9c0b-81750b8c3e40")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.*")]
-[assembly: AssemblyFileVersion("2.1")]
+// Version Information via GlobalAssemblyInfo.cs
--- src/KeePassCommanderPlugin/Properties/AssemblyInfo.cs
+++ src/KeePassCommanderPlugin/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("7c5e6684-329b-450c-8f8e-e2d344004816")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+// Version Information via GlobalAssemblyInfo.cs
