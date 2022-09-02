5e005ca15373616c3e46476b3517d4f7ed6d71d1
Mirco Babin <mirco.babin@gmail.com>
2022-09-02 16:38:29 +0200

    KeePassEntry.php - added fields and attachments

--- src/ProgrammingLanguagesConnectors/KeePassEntry.php
+++ src/ProgrammingLanguagesConnectors/KeePassEntry.php
@@ -45,7 +45,18 @@ class KeePassEntry
     public $urlpath;
     public $notes;
     
-    public function __construct($entryname) {
+    public $fields;
+    
+    public $attachments;
+    
+    /* 
+    options = (null | array with each key optional) [ 
+        'KeePassCommand.exe' => (string) full path and filename of KeePassCommand.exe e.g. 'c:\KeePass\KeePassCommand.exe'
+        'FieldNames' => (array) array of fieldnames to retrieve. e.g. ['field1', 'field2', ...]
+        'AttachmentNames' = (array) array of attachmentnames to retrieve. e.g. ['file.txt', 'key-private.pem', 'key-public.pem', ...]
+    ]
+    */
+    public function __construct($entryname, $options = null) {
         $this->title = '';
         $this->username = '';
         $this->password = '';
@@ -56,14 +67,54 @@ class KeePassEntry
         $this->urlpath = '';
         $this->notes = '';
         
-        $cmd = str_replace('/', '\\', __DIR__ . '/KeePassCommand.exe');
+        $this->fields = array();
+        
+        $this->attachments = array();
+        
+        $cmd = null;
+        $fieldNames = null;
+        $attachmentNames = null;
+        if (is_array($options)) {
+            if (isset($options['KeePassCommand.exe']) && is_string($options['KeePassCommand.exe'])) {
+                $cmd = $options['KeePassCommand.exe'];
+            }
+            
+            if (isset($options['FieldNames']) && is_array($options['FieldNames'])) {
+                $fieldNames = $options['FieldNames'];
+            }
+            
+            if (isset($options['AttachmentNames']) && is_array($options['AttachmentNames'])) {
+                $attachmentNames = $options['AttachmentNames'];
+            }
+        }
+
+        if ($cmd === null) {
+            $cmd = __DIR__ . '/KeePassCommand.exe';
+        }
+        $cmd = str_replace('/', '\\', $cmd);
         if (!file_exists($cmd)) {
             throw new \Exception('KeePassCommand.exe not found: ' . $cmd);
         }
-        $cmd .= ' get "' . $entryname . '"';
+        $cmd = '"'.$cmd.'"';
+        
+        $this->Get($cmd, $entryname);
+        if ($this->title === $entryname) {
+            if (is_array($fieldNames)) {
+                $this->GetField($cmd, $fieldNames);
+            }
+            
+            if (is_array($attachmentNames)) {
+                $this->GetAttachment($cmd, $attachmentNames);
+            }
+        }
+    }
+        
+    private function Get($cmd, $entryname) {
+        $cmd .= ' get ' . escapeshellarg($entryname);
         $output = shell_exec($cmd);
         
         $lines = explode("\n", $output);
+        $titleFound = false;
         $state = 0;
         foreach($lines as $line) {
             switch($state) {
@@ -80,6 +131,7 @@ class KeePassEntry
                             case 1: 
                                 if ($value === $entryname) {
                                     $this->title = $value;
+                                    $titleFound = true;
                                     $state++;
                                 } else {
                                     $state = 0;
@@ -123,7 +175,113 @@ class KeePassEntry
                         }
                     } else if (substr($line, 0, 2) === "E\t") {
                         $state = 0;
-                        if ($this->title !== '') break;
+                        if ($titleFound) break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    private function GetField($cmd, $fieldNames) {
+        $cmd .= ' getfield ' . escapeshellarg($this->title);
+        foreach($fieldNames as $name) {
+            $cmd .= ' '.escapeshellarg($name);
+        }
+        $output = shell_exec($cmd);
+        
+        $lines = explode("\n", $output);
+        $titleFound = false;
+        $state = 0;
+        foreach($lines as $line) {
+            switch($state) {
+                case 0:
+                    if (substr($line, 0, 2) === "B\t") {
+                        $state = 1;
+                    }
+                    break;
+                default:
+                    if (substr($line, 0, 2) === "I\t") {
+                        $value = substr($line,2);
+                        $p = strpos($value, "\t");
+                        if ($p !== false) {
+                            $name = substr($value, 0, $p);
+                            $value = substr($value, $p+1);
+                        } else {
+                            $name = '';
+                            $value = '';
+                        }
+                        
+                        switch($state) {
+                            case 1: 
+                                if ($name === 'title' && $value === $this->title) {
+                                    $titleFound = true;
+                                    $state++;
+                                } else {
+                                    $state = 0;
+                                }
+                                break;
+                            case 2:
+                                if ($name !== '') {
+                                    $this->fields[$name] = $value;
+                                }
+                                break;
+                        }
+                    } else if (substr($line, 0, 2) === "E\t") {
+                        $state = 0;
+                        if ($titleFound) break;
+                    }
+                    break;
+            }
+        }
+    }
+    
+    private function GetAttachment($cmd, $attachmentNames) {
+        $cmd .= ' getattachment ' . escapeshellarg($this->title);
+        foreach($attachmentNames as $name) {
+            $cmd .= ' '.escapeshellarg($name);
+        }
+        $output = shell_exec($cmd);
+        
+        $lines = explode("\n", $output);
+        $titleFound = false;
+        $state = 0;
+        foreach($lines as $line) {
+            switch($state) {
+                case 0:
+                    if (substr($line, 0, 2) === "B\t") {
+                        $state = 1;
+                    }
+                    break;
+                default:
+                    if (substr($line, 0, 2) === "I\t") {
+                        $value = substr($line,2);
+                        $p = strpos($value, "\t");
+                        if ($p !== false) {
+                            $name = substr($value, 0, $p);
+                            $value = substr($value, $p+1);
+                        } else {
+                            $name = '';
+                            $value = '';
+                        }
+                        
+                        switch($state) {
+                            case 1: 
+                                if ($name === 'title' && $value === $this->title) {
+                                    $titleFound = true;
+                                    $state++;
+                                } else {
+                                    $state = 0;
+                                }
+                                break;
+                            case 2:
+                                if ($name !== '') {
+                                    $this->attachments[$name] = base64_decode($value);
+                                }
+                                break;
+                        }
+                    } else if (substr($line, 0, 2) === "E\t") {
+                        $state = 0;
+                        if ($titleFound) break;
                     }
                     break;
             }
