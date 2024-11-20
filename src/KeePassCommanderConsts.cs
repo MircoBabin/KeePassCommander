0564234441dba41f5dca5216b9763fc64e043782
Mirco Babin <mirco.babin@gmail.com>
2024-11-20 21:54:38 +0100

    add: KeePassCommanderConsts.EOL

---
+++ src/KeePassCommanderConsts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KeePassCommander
+{
+    public static class KeePassCommanderConsts
+    {
+        public const string EOL = "\r\n";
+    }
+}
